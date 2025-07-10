0fac016be6defd5bc44681f7bcb18baa6e7b05c8
geoenergia <nd203@leicester.ac.uk>
2025-07-10 10:08:00 -0300

    Fix main func

--- LBM.cu
+++ LBM.cu
@@ -46,13 +46,6 @@ int main(int argc, char *argv[])
         hostMoments.varNames(),
         mesh);
     device::halo blockHalo(hostMoments.arr(), mesh);
-    // const device::array<nodeType_t> nodeTypes(host::nodeType(mesh), {"nodeTypes"}, mesh);
-
-    // Set up time averaging
-    // device::array<scalar_t> momentsMean(
-    //     host::moments(mesh, programCtrl.u_inf()),
-    //     {"rhoMean", "uMean", "vMean", "wMean", "m_xxMean", "m_xyMean", "m_xzMean", "m_yyMean", "m_yzMean", "m_zzMean"},
-    //     mesh);
 
     // Copy symbols to device
     mesh.copyDeviceSymbols();
@@ -77,62 +70,21 @@ int main(int argc, char *argv[])
             deviceMoments.ptr(),
             blockHalo);
 
-        // momentBasedD3Q19_v2<<<mesh.gridBlock(), mesh.threadBlock(), 0, streamsLBM[0]>>>(
-        //     rho.ptr(),
-        //     u.ptr(),
-        //     v.ptr(),
-        //     w.ptr(),
-        //     m_xx.ptr(),
-        //     m_xy.ptr(),
-        //     m_xz.ptr(),
-        //     m_yy.ptr(),
-        //     m_yz.ptr(),
-        //     m_zz.ptr(),
-        //     blockHalo);
-
-        // checkCudaErrors(cudaDeviceSynchronize());
-        // fieldAverage::calculate<<<mesh.gridBlock(), mesh.threadBlock(), 0, streamsLBM[0]>>>(
-        //     deviceMoments.ptr(),
-        //     momentsMean.ptr(),
-        //     nodeTypes.ptr(),
-        //     timeStep);
-
         blockHalo.swap();
 
-        // if (programCtrl.save(timeStep))
-        // {
-        //     // deviceMoments.write(programCtrl.caseName(), timeStep);
-
-        // if (timeStep > 0)
-        // {
-        //     postProcess::writeTecplotHexahedralData(
-        //         fileIO::deinterleaveAoS(host::copyToHost(deviceMoments.ptr(), deviceMoments.size()), mesh),
-        //         programCtrl.caseName() + "_" + std::to_string(timeStep) + ".dat",
-        //         mesh,
-        //         deviceMoments.varNames(),
-        //         "Title");
-        // }
-
-        // momentsMean.write(programCtrl.caseName(), timeStep);
-
-        // postProcess::writeTecplotHexahedralData(
-        //     fileIO::deinterleaveAoS(host::copyToHost(momentsMean.ptr(), mesh.nPoints() * 10), mesh),
-        //     programCtrl.caseName() + "Mean_" + std::to_string(timeStep) + ".dat",
-        //     mesh,
-        //     momentsMean.varNames(),
-        //     "Title");
+        if (programCtrl.save(timeStep))
+        {
+            deviceMoments.write(programCtrl.caseName(), timeStep);
+        }
     }
 
-    // checkCudaErrors(cudaDeviceSynchronize());
-}
+    // Get ending time point and output the elapsed time
+    const std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    std::cout << std::endl;
+    std::cout << "Elapsed time: " << runTimeIO::duration(std::chrono::duration_cast<std::chrono::seconds>(end - start).count()) << std::endl;
+    std::cout << std::endl;
+    std::cout << "MLUPS: " << runTimeIO::MLUPS<double>(mesh, programCtrl, start, end) << std::endl;
+    std::cout << "End" << std::endl;
 
-// Get ending time point and output the elapsed time
-const std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-std::cout << std::endl;
-std::cout << "Elapsed time: " << runTimeIO::duration(std::chrono::duration_cast<std::chrono::seconds>(end - start).count()) << std::endl;
-std::cout << std::endl;
-std::cout << "MLUPS: " << std::setprecision(15) << runTimeIO::MLUPS<double>(mesh, programCtrl, start, end) << std::endl;
-std::cout << "End" << std::endl;
-
-return 0;
+    return 0;
 }

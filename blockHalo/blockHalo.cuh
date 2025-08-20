9582fe758d1b1e083e30a1e33210fd5bacf79481
gtchoaire <gtchoaire@gmail.com>
2025-08-20 14:06:57 -0300

    Significant clean-up

---
+++ blockHalo/blockHalo.cuh
@@ -0,0 +1,36 @@
+/**
+Filename: halo.cuh
+Contents: A class handling the device halo
+This class is used to exchange the microscopic velocity components at the edge of a CUDA block
+**/
+
+#ifndef __MBLBM_BLOCKHALO_CUH
+#define __MBLBM_BLOCKHALO_CUH
+
+#include "../LBMIncludes.cuh"
+#include "../LBMTypedefs.cuh"
+#include "../globalFunctions.cuh"
+#include "../velocitySet/velocitySet.cuh"
+#include "../latticeMesh/latticeMesh.cuh"
+
+namespace LBM
+{
+    namespace device
+    {
+        namespace haloFaces
+        {
+            /**
+             * @brief Consteval functions used to distinguish between halo face normal directions
+             * @return An unsigned integer corresponding to the correct direction
+             **/
+            [[nodiscard]] static inline consteval std::size_t x() noexcept { return 0; }
+            [[nodiscard]] static inline consteval std::size_t y() noexcept { return 1; }
+            [[nodiscard]] static inline consteval std::size_t z() noexcept { return 2; }
+        }
+    }
+}
+
+#include "haloFace.cuh"
+#include "halo.cuh"
+
+#endif

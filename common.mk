aed0f9d318297613e2b7a7f615f13816ecf851ff
nd-92 <nd203@leicester.ac.uk>
2025-09-08 10:44:02 -0300

    Fix build process - now choose between automatic and manual arch detection

--- common.mk
+++ common.mk
@@ -6,7 +6,7 @@ CXX = g++
 
 # CUDA Compiler Flags
 NVCXX_STANDARD = -std c++20
-NVCXX_OPTFLAGS = -O3 --restrict
+NVCXX_OPTFLAGS = -O3 --restrict -extra-device-vectorization
 NVCXX_MFLAGS = --m64
 NVCXX_WFLAGS = --Wreorder --Wdefault-stream-launch --Wmissing-launch-bounds --Wext-lambda-captures-this -Xptxas -v
 NVCXX_COMPILER_WFLAGS = -Xcompiler "-O3 -funroll-loops -march=native -mtune=native -Wall -Wextra -Werror -Wattributes -Wbuiltin-macro-redefined -Wcast-align -Wconversion -Wdiv-by-zero -Wdouble-promotion -Wfloat-equal -Wformat-security -Wformat=2 -Wimplicit-fallthrough=5 -Winline -Wint-to-pointer-cast -Wlogical-op -Woverflow -Wpointer-arith -Wshadow -Wsign-conversion -Wstrict-aliasing=3 -Wstringop-overflow=4 -Wwrite-strings"

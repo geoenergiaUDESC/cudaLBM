346a54f13b43c1ec8bb9b91c198c3496b90df973
nd-92 <nd203@leicester.ac.uk>
2025-10-20 16:42:04 -0300

    Implement basics of cut plane

--- src/latticeMesh/latticeMesh.cuh
+++ src/latticeMesh/latticeMesh.cuh
@@ -210,21 +210,7 @@ namespace LBM
                   L_(
                       {string::extractParameter<scalar_t>(string::readFile("latticeMesh"), "Lx"),
                        string::extractParameter<scalar_t>(string::readFile("latticeMesh"), "Ly"),
-                       string::extractParameter<scalar_t>(string::readFile("latticeMesh"), "Lz")}){};
-
-            __host__ [[nodiscard]] latticeMesh(const host::latticeMesh &mesh, const blockLabel_t meshDimensions) noexcept
-                : nx_(meshDimensions.nx),
-                  ny_(meshDimensions.ny),
-                  nz_(meshDimensions.nz),
-                  nPoints_(nx_ * ny_ * nz_),
-                  L_(mesh.L()){};
-
-            __host__ [[nodiscard]] latticeMesh(const host::latticeMesh &mesh) noexcept
-                : nx_(mesh.nx()),
-                  ny_(mesh.ny()),
-                  nz_(mesh.nz()),
-                  nPoints_(nx_ * ny_ * nz_),
-                  L_(mesh.L()){};
+                       string::extractParameter<scalar_t>(string::readFile("latticeMesh"), "Lz")}) { ; }
 
             /**
              * @name Grid Dimension Accessors
@@ -295,6 +281,15 @@ namespace LBM
             __host__ [[nodiscard]] inline constexpr const pointVector &L() const noexcept
             {
                 return L_;
+            }
+
+            /**
+             * @brief Get the number of physical dimensions of the mesh
+             * @return Const reference to pointVector containing domain size
+             **/
+            __host__ [[nodiscard]] inline constexpr label_t nDims() const noexcept
+            {
+                return static_cast<label_t>(nx_ > 1) + static_cast<label_t>(ny_ > 1) + static_cast<label_t>(nz_ > 1);
             }
 
             /**

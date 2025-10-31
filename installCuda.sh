391d86e9ae54753c4c302a8e6b6973b887ad321f
nd-92 <nd203@leicester.ac.uk>
2025-10-31 11:03:04 -0300

    CUDA install script

---
+++ installCuda.sh
@@ -0,0 +1,10 @@
+# For Ubuntu-24.04
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y build-essential
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt install -y cuda-toolkit-13-0  # For CUDA 12.9
+echo 'export PATH="/usr/local/cuda-13-0/bin:$PATH"' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH="/usr/local/cuda-13-0/lib64:$LD_LIBRARY_PATH"' >> ~/.bashrc
+source ~/.bashrc

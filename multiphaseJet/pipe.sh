b5f034763c659d99e4b18ced1eb0448a2d130aa9
kreeyto <royal.ssamy@gmail.com>
2026-01-08 19:45:55 -0300

    remove: pipe.sh

--- multiphaseJet/pipe.sh
+++
@@ -1,5 +0,0 @@
-./cleanCase.sh
-
-multiphaseD3Q19 -GPU 0
-
-fieldConvert -fileType vts
--- pipe.sh
+++
@@ -1,9 +0,0 @@
-make install
-
-cd multiphaseJet
-
-source cleanCase.sh
-
-multiphaseD3Q19 -GPU 0
-
-fieldConvert -fileType vts
